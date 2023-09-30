6fe5dd1942c61cea505e379a60e775746e01409a
Mike Nitsenko <mnitsenko@gmail.com>
2023-09-30 21:54:36 +0600

    upd config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // LOCAL
 module.exports = {
 
-    videosBaseDir: "C:/dev/HCSBK/videos/",
+    videosBaseDir: "C:/dev/videos/",
     ffmpegPath: "C:/dev/ffmpeg/ffmpeg-4.4-full_build/bin/ffmpeg.exe",
     ffmpegThreads: 4,
     //mjrConverterScript: "bash /storage/recordings/convert-mjr.sh"
